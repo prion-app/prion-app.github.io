fa563239f1dd8f88f00ef3f012e0a15676a5327b
Tobias Brander <tobias.sundstrand@gmail.com>
2017-11-22 16:56:15 +0100

    Contact form

--- js/agency.js
+++ js/agency.js
@@ -51,12 +51,29 @@ $('form[id=contactForm]').submit(function(){
   $.post($(this).attr('action'), $(this).serialize(), function(res){
     $('form[id=contactForm] #success').hide();
     $('form[id=contactForm] #error').hide();
-    if (res.code == "200" || res.code == "302")
+    if (res.success) {
       $('form[id=contactForm] #success').show();
-    }).fail(function(){
+    }
+  }, 'json').fail(function(){
     $('form[id=contactForm] #success').hide();
     $('form[id=contactForm] #error').hide();
     $('form[id=contactForm] #success').show();
+  });
+  return false;
+});
+
+// Async news form
+$('form[id=newsForm]').submit(function(){
+  $.post($(this).attr('action'), $(this).serialize(), function(res){
+    $('form[id=newsForm] #success').hide();
+    $('form[id=newsForm] #error').hide();
+    if (res.success) {
+      $('form[id=newsForm] #success').show();
+    }
+  }, 'json').fail(function(){
+    $('form[id=newsForm] #success').hide();
+    $('form[id=newsForm] #error').hide();
+    $('form[id=newsForm] #success').show();
   });
   return false;
 });
