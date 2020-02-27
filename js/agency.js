e01f265c76b7ec17adeaa0ddc2a2302204f1a650
Eskil Andreen <eskil@ndrn.se>
2020-02-27 11:07:22 +0100

    Deploy version (66fa760)

--- js/agency.js
+++ js/agency.js
@@ -49,15 +49,16 @@ $('.navbar-collapse ul li a').click(function() {
 // Async contact form
 $('form[id=contactForm]').submit(function(){
   $.post($(this).attr('action'), $(this).serialize(), function(res){
-    $('form[id=contactForm] #success').hide();
-    $('form[id=contactForm] #error').hide();
-    if (res.success) {
+    if (res.ok) {
+      $('form[id=contactForm] #error').hide();
       $('form[id=contactForm] #success').show();
+    } else {
+      $('form[id=contactForm] #success').hide();
+      $('form[id=contactForm] #error').show();
     }
   }, 'json').fail(function(){
     $('form[id=contactForm] #success').hide();
-    $('form[id=contactForm] #error').hide();
-    $('form[id=contactForm] #success').show();
+    $('form[id=contactForm] #error').show();
   });
   return false;
 });
@@ -65,15 +66,16 @@ $('form[id=contactForm]').submit(function(){
 // Async news form
 $('form[id=newsForm]').submit(function(){
   $.post($(this).attr('action'), $(this).serialize(), function(res){
-    $('form[id=newsForm] #success').hide();
-    $('form[id=newsForm] #error').hide();
-    if (res.success) {
+    if (res.ok) {
+      $('form[id=newsForm] #error').hide();
       $('form[id=newsForm] #success').show();
+    } else {
+      $('form[id=newsForm] #success').hide();
+      $('form[id=newsForm] #error').show();
     }
   }, 'json').fail(function(){
     $('form[id=newsForm] #success').hide();
-    $('form[id=newsForm] #error').hide();
-    $('form[id=newsForm] #success').show();
+    $('form[id=newsForm] #error').show();
   });
   return false;
 });
@@ -81,15 +83,16 @@ $('form[id=newsForm]').submit(function(){
 // Async news form
 $('form[id=preschoolContactModal]').submit(function(){
   $.post($(this).attr('action'), $(this).serialize(), function(res){
-    $('form[id=preschoolContactModal] #success').hide();
-    $('form[id=preschoolContactModal] #error').hide();
     if (res.success) {
+      $('form[id=preschoolContactModal] #error').hide();
       $('form[id=preschoolContactModal] #success').show();
+    } else {
+      $('form[id=preschoolContactModal] #success').hide();
+      $('form[id=preschoolContactModal] #error').show();
     }
   }, 'json').fail(function(){
     $('form[id=preschoolContactModal] #success').hide();
-    $('form[id=preschoolContactModal] #error').hide();
-    $('form[id=preschoolContactModal] #success').show();
+    $('form[id=preschoolContactModal] #error').show();
   });
   return false;
 });
